0db78660f1d992f8286d997a0aa6254ac001c232
zaga <163298261+zaga001@users.noreply.github.com>
2024-12-01 15:23:30 +0700

    Add displayBook function

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const myLibrary = [];
+const myLibrary = [{
+    title: "1984",
+    author: "George Orwell",
+    pages: 328,
+    read: true
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    pages: 281,
+    read: false
+  }];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -12,15 +23,19 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
-const heading = document.querySelector('#heading');
-const tr = document.createElement('tr');
-const td = document.createElement('td');
+const table = document.querySelector('table');
 
 function displayBook(array) {
-    array.forEach(element => {
-
+    array.forEach(object => {
+        const template = `
+            <tr>
+                <td>${object.title}</td>
+                <td>${object.author}</td>
+                <td>${object.pages}</td>
+                <td>${object.read}</td>
+            </tr>`;
+        table.innerHTML += template;
     });
 }
 
-addBookToLibrary('Dune', 'Frank Herbert', 100, true);
 displayBook(myLibrary);
