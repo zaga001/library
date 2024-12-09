432d667be28c775958be58d3b5a15645f3269c12
zaga <163298261+zaga001@users.noreply.github.com>
2024-12-09 13:17:06 +0700

    Add new book function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const myLibrary = [{
     title: "1984",
     author: "George Orwell",
-    pages: 328,
+    pages: '328',
     read: true
   },
   {
     title: "To Kill a Mockingbird",
     author: "Harper Lee",
-    pages: 281,
+    pages: '281',
     read: false
   }];
 
@@ -23,7 +23,7 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
-const table = document.querySelector('table');
+const table = document.querySelector('tbody');
 
 function displayBook(array) {
     array.forEach(object => {
@@ -39,3 +39,25 @@ function displayBook(array) {
 }
 
 displayBook(myLibrary);
+
+const openBtn = document.querySelector('#open-modal');
+const dialog = document.querySelector('#dialog');
+const closeBtn = document.querySelector('#close-modal');
+const submit = document.querySelector('#submit');
+
+openBtn.addEventListener('click', () => dialog.showModal());
+closeBtn.addEventListener('click', () => dialog.close());
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const read = document.querySelector('input[name="read"]:checked').value === 'true';
+    addBookToLibrary(title, author, pages, read);
+    table.innerHTML = '';
+    displayBook(myLibrary);
+    const form = document.querySelector('form');
+    form.reset();
+    dialog.close();
+});
