9ee3930ccd51c11262954eb28ece807ba43d7dd8
zaga <163298261+zaga001@users.noreply.github.com>
2024-12-10 08:05:10 +0700

    Finish library

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-const myLibrary = [{
-    title: "1984",
-    author: "George Orwell",
-    pages: '328',
-    read: true
-  },
-  {
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    pages: '281',
-    read: false
-  }];
+const myLibrary = [
+    new Book("Omniscient Reader's Viewpoint", "Sing Shong", 551, true),
+    new Book("Lord of the Mysteries", "Cuttlefish That Loves Diving", 1430, true)
+];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -17,6 +9,10 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+
+Book.prototype.toggleReadStatus = function() {
+    this.read = !this.read;
+};
 
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
@@ -26,24 +22,70 @@ function addBookToLibrary(title, author, pages, read) {
 const table = document.querySelector('tbody');
 
 function displayBook(array) {
-    array.forEach(object => {
+    table.innerHTML = '';
+    array.forEach((object, index) => {
         const template = `
-            <tr>
+            <tr data-index="${index}">
                 <td>${object.title}</td>
                 <td>${object.author}</td>
                 <td>${object.pages}</td>
                 <td>${object.read}</td>
+                <td>
+                    <button class="toggle-read-btn" data-index="${index}">
+                        Toggle Read
+                    </button>
+                </td>
+                <td>
+                    <button class="remove-btn" data-index="${index}">
+                        Remove
+                    </button>
+                </td>
             </tr>`;
         table.innerHTML += template;
     });
+    
+    addRemoveEventListeners();
+    addToggleReadEventListeners();
 }
 
-displayBook(myLibrary);
+function addRemoveEventListeners() {
+    const removeButtons = document.querySelectorAll('.remove-btn');
+    removeButtons.forEach(button => {
+        button.removeEventListener('click', removeButtonClickHandler);
+        button.addEventListener('click', removeButtonClickHandler);
+    });
+}
+
+function addToggleReadEventListeners() {
+    const toggleReadButtons = document.querySelectorAll('.toggle-read-btn');
+    toggleReadButtons.forEach(button => {
+        button.removeEventListener('click', toggleReadButtonClickHandler);
+        button.addEventListener('click', toggleReadButtonClickHandler);
+    });
+}
+
+function removeButtonClickHandler(event) {
+    const index = event.target.getAttribute('data-index');
+    removeBookFromLibrary(index);
+}
+
+function toggleReadButtonClickHandler(event) {
+    const index = event.target.getAttribute('data-index');
+    myLibrary[index].toggleReadStatus();
+    displayBook(myLibrary);
+}
+
+function removeBookFromLibrary(index) {
+    myLibrary.splice(index, 1);
+    displayBook(myLibrary);
+}
 
 const openBtn = document.querySelector('#open-modal');
 const dialog = document.querySelector('#dialog');
 const closeBtn = document.querySelector('#close-modal');
 const submit = document.querySelector('#submit');
+
+displayBook(myLibrary);
 
 openBtn.addEventListener('click', () => dialog.showModal());
 closeBtn.addEventListener('click', () => dialog.close());
@@ -55,7 +97,6 @@ submit.addEventListener('click', (event) => {
     const pages = document.querySelector('#pages').value;
     const read = document.querySelector('input[name="read"]:checked').value === 'true';
     addBookToLibrary(title, author, pages, read);
-    table.innerHTML = '';
     displayBook(myLibrary);
     const form = document.querySelector('form');
     form.reset();
